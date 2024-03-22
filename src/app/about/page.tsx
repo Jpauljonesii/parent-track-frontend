0d47cf9b2645c81b9b9b9e717f54329c81b185c8
John P. Jones II <jpauljonesii@gmail.com>
2024-03-22 00:44:05 -0500

    initial setup connecting to backend repo

---
+++ src/app/about/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// @ts-ignore
+function page(props) {
+  return (
+
+    <>
+    
+    <h1> 
+    About the Parent Tracker
+    
+    </h1>
+        
+    
+    </>
+
+
+  );
+    
+  
+}
+
+
+
+export default page
